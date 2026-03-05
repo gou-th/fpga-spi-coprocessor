fbe83af86fd6a28d0034b54cfcefba992c76ffdd
Goutham Ganesh Kumar <87598822+gou-th@users.noreply.github.com>
2026-03-05 22:59:47 +0530

    Added comments in ALU module

--- rtl/alu.sv
+++ rtl/alu.sv
@@ -7,12 +7,13 @@ module alu(
     output logic [31:0] result
 );
 
-    logic [31:0] acc=0;
+    logic [31:0] acc=0;            //Accumulator for MAC operation 
     logic [31:0] last_result=0;
 
     logic [7:0] opcode;
     logic [7:0] op_a,op_b;
     
+    //Unpacking 24-bit packet 
     assign opcode=pkt_in[23:16];
     assign op_a=pkt_in[15:8];
     assign op_b=pkt_in[7:0];
@@ -24,14 +25,15 @@ module alu(
                 8'h02: last_result<=op_a*op_b;
                 8'h03: last_result<=op_a&op_b;
                 8'h04: last_result<=op_a^op_b;
-                8'h05: acc<=acc+(op_a*op_b);
-                8'h06: acc<=0;
+                8'h05: acc<=acc+(op_a*op_b);     //MAC operation 
+                8'h06: acc<=0;                    
                 8'h09: last_result<=(acc==0)?0:acc;
                 default: ;
             endcase
         end
     end
-
+    
+    //Output routing 
     always_comb begin
         case (opcode)
             8'h07:result=acc;
