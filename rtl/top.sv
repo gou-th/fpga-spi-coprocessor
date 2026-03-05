0132533049f8fcbcb6560fd27a0a7ec74b9276ec
Goutham Ganesh Kumar <87598822+gou-th@users.noreply.github.com>
2026-03-05 23:11:09 +0530

    Add logic for read command and instantiate modules

--- rtl/top.sv
+++ rtl/top.sv
@@ -16,12 +16,14 @@ module top(
     logic [7:0] opcode;
     assign opcode=pkt_data[23:16];
 
+    //Logic to check if command requires sending data back 
     logic is_read_cmd;
     assign is_read_cmd=pkt_ready&&((opcode==8'h07)||(opcode==8'h08));
 
     logic [31:0] data;
     logic load;
 
+    //Instantiate the receiver module 
     spi_receiver receiver_inst (
         .clk (clk100mhz),
         .sck_in (sck_in),
@@ -30,7 +32,8 @@ module top(
         .pkt (pkt_data),
         .ready (pkt_ready)
     );
-    
+
+    //instantiate the ALU module
     alu alu_inst (
         .clk (clk100mhz),
         .pkt_in (pkt_data),
@@ -38,7 +41,7 @@ module top(
         .result (alu_out)
     );
 
-
+    //Transmitter load logic 
     always_ff @(posedge clk100mhz) begin
         load<=0;        
         if (cs_in) begin
@@ -50,6 +53,7 @@ module top(
         end
     end
 
+    //Instantiate the transmitter module 
     spi_transmitter transmitter_inst (
         .clk (clk100mhz),
         .sck_in (sck_in),
@@ -59,6 +63,7 @@ module top(
         .miso (miso_out)
     );
 
+    //Mapping lower half of ALU result to Basys 3 FPGA LEds
     always_ff @(posedge clk100mhz) begin
         if (pkt_ready)
             led<=alu_out[15:0];
